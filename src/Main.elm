84547dd9ae908d8f2758609447302eba6fd9447d
tekihei2317 <tekihei2317@gmail.com>
2022-11-08 21:00:28 +0900

    feat: ステージ上にモノ（壁、荷物、ゴール、キャラクター）を配置

--- src/Main.elm
+++ src/Main.elm
@@ -2,15 +2,37 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (class)
 
 
+main : Program () Model Msg
 main =
-    Browser.sandbox { init = { counter = 0 }, update = update, view = view }
+    Browser.sandbox { init = initialModel, update = update, view = view }
+
+
+initialModel : Model
+initialModel =
+    { counter = 0
+    , stage = initialStage
+    }
+
+
+initialStage : List String
+initialStage =
+    [ "#####"
+    , "#@  #"
+    , "# $ #"
+    , "### #"
+    , "#.$ #"
+    , "#  .#"
+    , "#####"
+    ]
 
 
 type alias Model =
-    { counter : Int }
+    { counter : Int
+    , stage : List String
+    }
 
 
 type Msg
@@ -28,10 +50,71 @@ update msg model =
             { model | counter = model.counter - 1 }
 
 
+type Cell
+    = Player
+    | Wall
+    | Box
+    | Objective
+    | Empty
+
+
+stringToCell : String -> Cell
+stringToCell str =
+    if str == "@" then
+        Player
+
+    else if str == "#" then
+        Wall
+
+    else if str == "$" then
+        Box
+
+    else if str == "." then
+        Objective
+
+    else
+        Empty
+
+
+cellClass : Cell -> String
+cellClass cell =
+    case cell of
+        Player ->
+            "player"
+
+        Wall ->
+            "wall"
+
+        Box ->
+            "box"
+
+        Objective ->
+            "objective"
+
+        Empty ->
+            "empty"
+
+
+cn : List String -> String
+cn classNames =
+    String.join " " classNames
+
+
+stageCell : String -> Html msg
+stageCell cellString =
+    let
+        cell =
+            cellString |> stringToCell
+    in
+    div [ class (cn [ "cell", cellClass cell ]) ] []
+
+
+stageLine : String -> Html msg
+stageLine line =
+    div [ class "board-row" ] (List.map stageCell (String.split "" line))
+
+
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model.counter) ]
-        , button [ onClick Increment ] [ text "+" ]
-        ]
+        (List.map stageLine model.stage)
